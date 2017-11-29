14497cf19dc2b833acba372e5328ca0b78f47555
Nasrudin Salim <nasrudin.salim.suden@gmail.com>
2017-11-29 22:54:51 +0800

    Weights init function for model

--- A3CModel.py
+++ A3CModel.py
@@ -8,6 +8,23 @@ def norm_col_init(weights, std=1.0):
     x *= std / torch.sqrt((x**2).sum(1, keepdim=True))
     return x
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = np.prod(weight_shape[1:4])
+        fan_out = np.prod(weight_shape[2:4]) * weight_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform_(-w_bound, w_bound)
+        m.bias.data.fill_(0)
+    elif classname.find('Linear') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = weight_shape[1]
+        fan_out = weight_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform_(-w_bound, w_bound)
+        m.bias.data.fill_(0)
+
 class A3Clstm(torch.nn.Module):
     def __init__(self, num_inputs, action_space):
         super(A3Clstm, self).__init__()
