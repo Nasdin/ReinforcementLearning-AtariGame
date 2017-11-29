1890949e0e5a3117c340204b51487ceb9540a933
Nasrudin Salim <nasrudin.salim.suden@gmail.com>
2017-11-29 22:21:40 +0800

    Add A3C Pytorch model with LSTM

---
+++ A3CModel.py
@@ -0,0 +1,62 @@
+import torch
+from torch.multiprocessing import Process
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+import torch.optim as optim
+
+
+class A3Clstm(torch.nn.Module):
+    def __init__(self, num_inputs, action_space):
+        super(A3Clstm, self).__init__()
+
+        # convolutional neural networks
+        self.conv1 = nn.Conv2d(num_inputs, 32, 5, stride=1, padding=2)
+        self.maxp1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 32, 5, stride=1, padding=1)
+        self.maxp2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(32, 64, 4, stride=1, padding=1)
+        self.maxp3 = nn.MaxPool2d(2, 2)
+        self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.maxp4 = nn.MaxPool2d(2, 2)
+
+        # LSTM Cells
+        self.lstm = nn.LSTMCell(1024, 512)
+        num_outputs = action_space.n
+
+        # The critic layer
+        self.critic_linear = nn.Linear(512, 1)
+        # The actor layer
+        self.actor_linear = nn.Linear(512, num_outputs)
+
+        self.apply(weights_init)
+        self.actor_linear.weight.data = norm_col_init(
+            self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = norm_col_init(
+            self.critic_linear.weight.data, 1.0)
+        self.critic_linear.bias.data.fill_(0)
+
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+
+        self.train()
+
+    # forward propagation
+    def forward(self, inputs):
+        inputs, (hx, cx) = inputs
+        x = F.relu(self.maxp1(self.conv1(inputs)))
+        x = F.relu(self.maxp2(self.conv2(x)))
+        x = F.relu(self.maxp3(self.conv3(x)))
+        x = F.relu(self.maxp4(self.conv4(x)))
+
+        x = x.view(x.size(0), -1)
+
+        hx, cx = self.lstm(x, (hx, cx))
+
+        x = hx
+
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
